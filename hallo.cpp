edaebceea938dd603dbe78a8ceffd07b45e139ab
Lampukaka2020 <helman.hrr@gmail.com>
2025-05-27 01:48:10 +0800

    Validasi Input

--- hallo.cpp
+++ hallo.cpp
@@ -9,4 +9,7 @@ int main (){
     cout <<"Masukkan jumlah nama yang akan diproses: ";
     cin >> jumlahNama;
 
-    
+    // Validasi input
+    if (jumlahNama <=0){
+      cout << "Jumlah nama harus lebih dari 0!-" <<endl;
+      return 1;
