d7f1b37d975da6fcb7aaa5b80aa4c204dc07be81
Lampukaka2020 <helman.hrr@gmail.com>
2025-05-27 01:34:03 +0800

    meminta pengguna memasukkan jumlah nama

--- hallo.cpp
+++ hallo.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-  cout << "Hello";
-  return 0;
+int main (){
+  int jumlahNama;
+
+    // Meminta pengguna memasukkan jumlah nama
+    cout <<"Masukkan jumlah nama yang akan diproses: ";
+    cin >> jumlahNama;
+
+    // Validasi input
+    if (jumlahNama <=0){
+      cout << "Jumlah nama harus lebih dari 0!-" <<endl;
+      return 1;
+    }
+    // Membersihkan karakter newline sisa dari cin >> Jumlahnama
+    cin.ignore();
+
+    // Inisialisasi array dinamis dengan jumlah nama
+    string* nama = new string [jumlahNama];
+
+     // Menampilkan data dari array
+     cout <<"\nDaftar Nama yang Dimasukkan:" << endl;
+     for (int i = 0; i < jumlahNama; i++){
+      cout << "Nama ke-" << i + 1 << ":";
+      getline(cin, nama[i]);
+     }
+     // Membersihkan memori yang dialokasikan
+     delete[] nama;
+
+     return 0;
 }
+
